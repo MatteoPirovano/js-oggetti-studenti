818eeeb637e0490ec81310fd06bea31bea2a2155
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-27 17:47:10 +0100

    quarto commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà;
-// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà; F
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome; F
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 $(document).ready(function() {
@@ -20,17 +20,17 @@ var elencoStudenti = [
   {
     "nome": 'Francesco',
     "cognome": 'Santero',
-    eta: 30,
+    'eta': 30,
   },
   {
     "nome": 'Manuel',
     "cognome": 'Lambruschi',
-    eta: 22,
+    'eta': 22,
   },
   {
     "nome": 'Mattia',
     "cognome": 'Salemi',
-    eta: 21
+    'eta': 21
   }
 ];
 
@@ -41,5 +41,11 @@ for (var i = 0; i < elencoStudenti.length; i++) {
   document.getElementById('nomi_cognomi').innerHTML+= nome + ' '+ cognome + '<br>' ;
 };
 
-
+var name = 'nome';
+elencoStudenti.name = prompt('inserisci il tuo nome');
+var surname = 'cognome';
+elencoStudenti.surname = prompt('inserisci il tuo cognome');
+var age = 'eta'
+ elencoStudenti.age = parseInt(prompt('inserisci la tua eta'));
+console.log(elencoStudenti);
 });
